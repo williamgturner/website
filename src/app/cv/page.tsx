c74345e3a5b71a5c47c870401b8d07724090b3c5
William Turner <williamguyturner@proton.me>
2025-09-11 12:06:26 +1200

    update cv

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -2,7 +2,7 @@ export default function Cv() {
     return (
     <div className="flex flex-1">
         <iframe
-        src="/William_Turner_CV.pdf"
+        src="/cv.pdf"
         width="50%"
         height="90%"
         className="mx-auto my-auto border-4 border-[#333]"        />
